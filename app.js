4a22ec84e6efe9a269a8f96e4af52e0eb94a1c55
foncheto <81327113+foncheto@users.noreply.github.com>
2023-07-21 20:29:28 -0400

    update win

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
 const app = express();
 const path = require("path");
 const Campground = require("./models/campground");
@@ -18,6 +19,7 @@ db.once("open", () => console.log("Database connected"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
